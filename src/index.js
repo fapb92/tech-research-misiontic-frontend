69ee87b9cec7076bfa97a3d575340f4fd6945336
fapb92 <69609369+fapb92@users.noreply.github.com>
2021-12-09 20:27:37 -0500

    Se ajusta index para poder hacer autenticacion

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './Authentication/Auth';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+})
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
